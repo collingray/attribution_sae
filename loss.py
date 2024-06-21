f3fd0d0d68a6ea33624cc061d15438babb84c53c
collin <me@collin.sh>
2024-06-21 12:11:24 -0700

    add loss metrics

--- loss.py
+++ loss.py
@@ -1,10 +1,38 @@
+import torch
 
 
+def reconstruction(x, y):
+    """
+    The MSE loss of the reconstruction
+    :param x: The input tensor
+    :param y: The reconstructed tensor
+    """
+    return ((y - x) ** 2).mean()
 
-def reconstruction(x, x_hat):
-    return ((x_hat - x) ** 2).mean(dim=-1)
 
-def sparsity(f):
-    return f.abs().mean(dim=-1)
+def act_sparsity(f):
+    """
+    The sparsity loss of the activations
+    :param f: The feature activations
+    """
+    return f.abs().sum(-1).mean()
 
-def
+
+def grad_sparsity(f, grad, dictionary):
+    """
+    The sparsity loss of the gradients
+    :param f: The feature activations
+    :param grad: The gradient of the LLM's loss w.r.t. the activations
+    :param dictionary: The autoencoder dictionary, i.e. the decoder weights
+    """
+    return (f * (grad @ dictionary)).abs().sum(-1).mean()
+
+
+def unexplained(x, y, grad):
+    """
+    The contribution of the unexplained variance to the loss of the LLM
+    :param x: The input tensor
+    :param y: The reconstructed tensor
+    :param grad: The gradient of the LLM's loss w.r.t. the activations
+    """
+    return ((x - y) * grad).sum(-1).abs().mean()
